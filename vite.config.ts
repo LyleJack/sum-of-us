5313588a75f7e6e6f65d13d0a7f9940898e3c76f
Ryan <95375350+LyleJack@users.noreply.github.com>
2026-06-02 13:57:37 +0100

    Add allowed host for development server to test cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     port: 5173,
     strictPort: true,
-    allowedHosts: ["localhost"],
+    allowedHosts: ["localhost",'sum-of-us-dev.sumofusdev.workers.dev'],
   },
 });
