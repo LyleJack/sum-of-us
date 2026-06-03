69e5e45247f575a43d5b38e609b62e23d5902409
LyleJack <lylejack75@gmail.com>
2026-06-03 11:45:54 +0100

    fix local run issues with cloudflare + adjust nav bar and welcome & class background image positions

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import { defineConfig } from "vite";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
-export default defineConfig({
-  plugins: [tailwindcss(), cloudflare()],
-  server: {
-    port: 5173,
-    strictPort: true,
-    allowedHosts: ["localhost",'sum-of-us-dev.sumofusdev.workers.dev'],
-  },
+export default defineConfig(({ command, mode }) => {
+  const plugins = [tailwindcss()];
+
+  if (command === "build" || mode === "cloudflare") {
+    plugins.push(cloudflare());
+  }
+
+  return {
+    plugins,
+    server: {
+      port: 5173,
+      strictPort: true,
+      allowedHosts: ["localhost", "sum-of-us-dev.sumofusdev.workers.dev"],
+    },
+  };
 });
