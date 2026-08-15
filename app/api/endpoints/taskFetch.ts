14f7306ca6c55d1fa80f277b37be87b03b60e6bc
Marios <marios1vasileiou@gmail.com>
2026-08-15 13:22:54 +0100

    init, not working yet

--- app/api/endpoints/taskFetch.ts
+++ app/api/endpoints/taskFetch.ts
@@ -1,55 +1,55 @@
-import { NotFoundException, OpenAPIRoute } from "chanfana";
-import { z } from "zod";
-import { type AppContext, Task } from "../types";
+// import { NotFoundException, OpenAPIRoute } from "chanfana";
+// import { z } from "zod";
+// import { type AppContext, Task } from "../types";
 
-export class TaskFetch extends OpenAPIRoute {
-	schema = {
-		tags: ["Tasks"],
-		summary: "Get a single Task by slug",
-		request: {
-			params: z.object({
-				taskSlug: z.string().describe("Task slug"),
-			}),
-		},
-		responses: {
-			"200": {
-				description: "Returns a single task if found",
-				content: {
-					"application/json": {
-						schema: z.object({
-							success: z.boolean(),
-							task: Task,
-						}),
-					},
-				},
-			},
-		},
-	};
+// export class TaskFetch extends OpenAPIRoute {
+// 	schema = {
+// 		tags: ["Tasks"],
+// 		summary: "Get a single Task by slug",
+// 		request: {
+// 			params: z.object({
+// 				taskSlug: z.string().describe("Task slug"),
+// 			}),
+// 		},
+// 		responses: {
+// 			"200": {
+// 				description: "Returns a single task if found",
+// 				content: {
+// 					"application/json": {
+// 						schema: z.object({
+// 							success: z.boolean(),
+// 							task: Task,
+// 						}),
+// 					},
+// 				},
+// 			},
+// 		},
+// 	};
 
-	async handle(c: AppContext) {
-		// Get validated data
-		const data = await this.getValidatedData<typeof this.schema>();
+// 	async handle(c: AppContext) {
+// 		// Get validated data
+// 		const data = await this.getValidatedData<typeof this.schema>();
 
-		// Retrieve the validated slug
-		const { taskSlug } = data.params;
+// 		// Retrieve the validated slug
+// 		const { taskSlug } = data.params;
 
-		// Implement your own object fetch here
+// 		// Implement your own object fetch here
 
-		const exists = true;
+// 		const exists = true;
 
-		if (!exists) {
-			throw new NotFoundException();
-		}
+// 		if (!exists) {
+// 			throw new NotFoundException();
+// 		}
 
-		return {
-			success: true,
-			task: {
-				name: "my task",
-				slug: taskSlug,
-				description: "this needs to be done",
-				completed: false,
-				due_date: new Date().toISOString().slice(0, 10),
-			},
-		};
-	}
-}
+// 		return {
+// 			success: true,
+// 			task: {
+// 				name: "my task",
+// 				slug: taskSlug,
+// 				description: "this needs to be done",
+// 				completed: false,
+// 				due_date: new Date().toISOString().slice(0, 10),
+// 			},
+// 		};
+// 	}
+// }
--- app/api/index.ts
+++ app/api/index.ts
@@ -1,26 +1,26 @@
-import { fromHono } from "chanfana";
-import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
+// import { fromHono } from "chanfana";
+// import { Hono } from "hono";
+// import { TaskCreate } from "./endpoints/taskCreate";
+// import { TaskDelete } from "./endpoints/taskDelete";
+// import { TaskFetch } from "./endpoints/taskFetch";
+// import { TaskList } from "./endpoints/taskList";
 
-// Start a Hono app
-const app = new Hono<{ Bindings: Env }>();
+// // Start a Hono app
+// const app = new Hono<{ Bindings: Env }>();
 
-// Setup OpenAPI registry
-const openapi = fromHono(app, {
-	docs_url: "/",
-});
+// // Setup OpenAPI registry
+// const openapi = fromHono(app, {
+// 	docs_url: "/",
+// });
 
-// Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+// // Register OpenAPI endpoints
+// openapi.get("/api/tasks", TaskList);
+// openapi.post("/api/tasks", TaskCreate);
+// openapi.get("/api/tasks/:taskSlug", TaskFetch);
+// openapi.delete("/api/tasks/:taskSlug", TaskDelete);
 
-// You may also register routes for non OpenAPI directly on Hono
-// app.get('/test', (c) => c.text('Hono!'))
+// // You may also register routes for non OpenAPI directly on Hono
+// // app.get('/test', (c) => c.text('Hono!'))
 
-// Export the Hono app
-export default app;
+// // Export the Hono app
+// export default app;
